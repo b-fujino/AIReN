1327f8c5760ad4256bf6ea25d5e64c140a9c3d76
b-fujino <b-fujino@g.fpu.ac.jp>
2026-02-21 05:03:49 +0900

    実験用AI完成

--- AIEngineForExperint.py
+++ AIEngineForExperint.py
@@ -12,12 +12,12 @@ from systemprompt_Agent_Experiment import INTERVIEWER_no_Guided, INTERVIEWER_Gui
 from systemprompt_Reporter import SCENARIO_J_2 as SCENARIO_J, REPORTER_J
 from systemprompt_IncidentReportGuide_Pydantic import IncidentReport_J as format_Report
 
-#from call_openai_api_Ollama import Agent_chat, Agent_chat_parsed, Agent_chat_tools
-from call_openai_api_openai import Agent_chat, Agent_chat_parsed
+from call_openai_api_Ollama import Agent_chat, Agent_chat_parsed, Agent_chat_tools
+#from call_openai_api_openai import Agent_chat, Agent_chat_parsed
 
 from pydantic import BaseModel, Field
 
-class CheckNewInfo(BaseModel):
+class CheckNewInfoModel(BaseModel):
     is_NewInfo: bool = Field(description="もし新しい情報が含まれていたら, true; もし特に新しい情報が含まれていなければ, false.")
 
 if __name__ == "__main__":
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 
     # 3. インタビューの実行
     ## 3.1 インタビュワーからの最初の質問の生成
-    message = [{"role": "user", "content": "自己紹介（名前と職業のみ）を行った上で，インタビューを開始してください．"}]
+    message = [{"role": "user", "content": "はじめまして．"}]
 
     Question = Agent_chat(
         system_prompt=INTERVIEWER_no_Guided,
@@ -39,6 +39,7 @@ if __name__ == "__main__":
 
     chatlog4interviewer = [{"role": "assistant", "content": Question}]
     chatlog4reporter = [{"role": "user", "content": Question}]   
+    summary = []
     ## 3.2 ループ処理
     while True:
         ### 3.2.1 レポーターからの回答の生成
@@ -53,24 +54,40 @@ if __name__ == "__main__":
         ### 3.2.2 新しい情報が含まれているかの判定
         CheckNewInfo = Agent_chat_parsed(
             system_prompt = f"あなたはコンテンツを分析し、新しい情報が含まれているかどうかを判断するエキスパートです。logの内容に対して，NewAnswerの内容が新しい情報を含んでいるかどうかを、is_NewInfoにtrue/falseで答えてください。",
-            message = [{"role": "user", "content": f"[log]：{chatlog4reporter}\n\n [NewAnswer]: {Answer}"}],
-            response_model=CheckNewInfo
+            messages = [{"role": "user", "content": f"[log]：{chatlog4reporter}\n\n [NewAnswer]: {Answer}"}],
+            format=CheckNewInfoModel
         )
         print("CheckNewInfo:")
-        print(json.dumps(CheckNewInfo.model_dump(), ensure_ascii=False, indent=2))
-        if not CheckNewInfo.is_NewInfo:
+        print(CheckNewInfo)
+        if not CheckNewInfo['is_NewInfo']:
             count+=1
 
         ### 3.2.3 カウントが一定数以上であればインタビューの終了．
         if count >= threshold:
             print("インタビューを終了します。")
+            print(f"最終的なSummary:")
+            for i, smry in enumerate(summary):
+                print(f"{i+1}. {smry}")
             break
 
-        ### 3.2.4 回答の記録
+
+        ### 3.2.4 サマリー作成
+        smry = Agent_chat(
+            system_prompt="あなたは優秀な要約者です．与えられたQuestionとAnswerから，何が明らかになったのかを1文にまとめて出力してください．",
+            messages=[
+                {"role": "user", "content": f"[Question]\n{Question}\n\n[Answer]\n{Answer}"},
+            ],
+            temperature=0.0
+        )
+        print(f"AI SUMMARIZER: ")
+        print(smry)
+        summary.append(smry)
+
+        ### 3.2.5 回答の記録
         chatlog4interviewer.append({"role": "user", "content": Answer})
         chatlog4reporter.append({"role": "assistant", "content": Answer})
 
-        ### 3.2.5 インタビュワーからの次の質問の生成
+        ### 3.2.6 インタビュワーからの次の質問の生成
         Question = Agent_chat(
             system_prompt=INTERVIEWER_Guided,
             messages=chatlog4interviewer
--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # AIReN
+
 AI for Reporting a Nearmiss and incident with HTML interface.
 
 ## Preparation
 
 ### uv
+
 install uv if before installing uv
+
 ```bash
 # macOS/Linux
 curl -LsSf https://astral.sh/uv/install.sh | sh
@@ -14,13 +17,17 @@ powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | ie
 ```
 
 ### viertual envrionment
+
 crate virtual environment
-```
+
+```bash
 uv venv
 ```
+
 in this case, folder name is set defalt name (.venv)
 
 Activate .venv
+
 ```bash
 # Linux
 source .venv/bin/activate
@@ -33,20 +40,21 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process # if need
 ### Required packages install
 
 install package
+
 ```bash
 uv pip install ollama dotenv requests flask flask_cors flask_socketio SpeechRecognition pydub
 ```
+
 **note**: ollama itself is necessary to be installed according to [ollama](https://ollama.com/download/linux).
 
-
 if use vllm, install required packages as follwoing:
-```
+
+```bash
 uv pip install torch torchvision --index-url https://download.pytorch.org/whl/cu129
 uv pip install vllm --torch-backend=auto
 ```
 
 ## Detail of Algorithm
-AIEngineCore.py is a main processing program of an interview. 
+
+AIEngineCore.py is a main processing program of an interview.
 [Flow Chart](AIEngineCoreFlowChart.md)
-
-
